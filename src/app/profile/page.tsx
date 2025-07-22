06db9131de4a2b0375462d8f7ebcfcb9994db4a0
Malaybhai <malay.raval11@gmail.com>
2025-07-22 18:44:15 +0530

    addind /profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+    return (
+        <div className="Header">
+            <h1 className="Head">Profile</h1>
+            <p className="text-gray-600">This is the profile page.</p>
+            <p className="text-gray-600">You can view and edit your profile information here.</p>
+        </div>
+    );
+}
